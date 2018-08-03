6787722430fc943ecbe194794991496d1e518693
Julio Cesar <julio.cesar@locaweb.com.br>
2018-08-03 16:03:57 -0300

    added test to show action, happy path

---
+++ COREMyCookBookAPITest/Controllers/Recipes/Show.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace COREMyCookBookAPITest.Controllers.Recipes
+{
+    public class Show : ControllerTestBase
+    {
+        [Test]
+        public void ShouldReturnARecipeWhenInvokeShowMethodWithId()
+        {
+            var size = 10;
+            var id = 4;
+            var recipeReturnedByTheRepository = BuilderRecipes(size).ElementAt(id);
+            _recipeRepositoryMock.Setup(c => c.GetById(id))
+                .Returns(recipeReturnedByTheRepository);
+
+            var recipeReturnedByTheController = _recipesController.Index();
+
+            Assert.AreEqual(recipeReturnedByTheRepository, recipeReturnedByTheController);
+        }
+    }
+}
