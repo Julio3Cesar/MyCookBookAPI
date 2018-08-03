36d60f197ee2cd87d4b6015361bf62d138a11f2d
Julio Cesar <julio.cesar@locaweb.com.br>
2018-08-03 16:36:29 -0300

    added bad path[

--- COREMyCookBookAPITest/Controllers/Recipes/Show.cs
+++ COREMyCookBookAPITest/Controllers/Recipes/Show.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using COREMyCookBookAPI.Models;
+using NUnit.Framework;
 using System.Linq;
 
 namespace COREMyCookBookAPITest.Controllers.Recipes
@@ -17,6 +18,34 @@ namespace COREMyCookBookAPITest.Controllers.Recipes
             var recipeReturnedByTheController = _recipesController.Show(id);
 
             Assert.AreEqual(recipeReturnedByTheRepository, recipeReturnedByTheController);
+        }
+
+        [Test]
+        public void ShouldReturnADifferentRecipeWhenInvokeShowMethodWithId()
+        {
+            var size = 10;
+            var id = 4;
+            var anotherId = 5;
+            var recipeReturnedByTheRepository = BuilderRecipes(size).ElementAt(id);
+            var anotherRecipe = BuilderRecipes(size).ElementAt(anotherId);
+            _recipeRepositoryMock.Setup(c => c.GetById(id))
+                .Returns(recipeReturnedByTheRepository);
+
+            var recipeReturnedByTheController = _recipesController.Show(id);
+
+            Assert.AreNotEqual(recipeReturnedByTheController, anotherRecipe);
+        }
+
+        [Test]
+        public void ShouldNotReturnARecipeWhenInvokeShowMethodWithIdThatNotExist()
+        {
+            var idThatNotExistInReposiitory = 11;
+            _recipeRepositoryMock.Setup(c => c.GetById(idThatNotExistInReposiitory))
+                .Returns<Recipe>(null);
+
+            var recipeReturnedByTheController = _recipesController.Show(idThatNotExistInReposiitory);
+
+            Assert.AreEqual(null, recipeReturnedByTheController);
         }
     }
 }
